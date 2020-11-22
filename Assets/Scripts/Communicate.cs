fdd1b288f2696d7a7e6ae890aae758bf111dd284
Ashley Papineau <design@ashleymuir.com>
2020-11-21 20:17:55 -0500

    Communicate detects distance

---
+++ Assets/Scripts/Communicate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Communicate : MonoBehaviour
+{
+
+    public float distanceThreshold = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject go = GameObject.Find ("PlayerShip");
+
+        Vector3 playerPosition = go.transform.position;
+        // Debug.Log (go.transform.position); // playership position
+
+        Vector3 alienPosition = transform.position;
+        // Debug.Log (transform.position); // alien ship position
+
+        float playerAlienDistance = Vector3.Distance(playerPosition, alienPosition);
+        // Debug.Log (playerAlienDistance);
+
+        if(playerAlienDistance <= distanceThreshold) {
+            Debug.Log ("In the zone!");
+        }
+    }
+}
