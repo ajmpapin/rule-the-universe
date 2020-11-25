84bfc524395b284f2fc229af6bc28cb73cb1e03f
Ashley Papineau <design@ashleymuir.com>
2020-11-24 23:15:21 -0500

    Player can loot power ups

---
+++ Assets/Scripts/LootPowerUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootPowerUp : MonoBehaviour
+{
+
+    int powerupAmt = 0;
+
+    void OnTriggerEnter2D(Collider2D entity) {
+        Debug.Log ("ADD MUSICAL CUE PLEASE");
+
+        if (entity.tag == "powerup") {
+            Destroy(entity.gameObject);
+
+            powerupAmt++;
+            Debug.Log ("you have " + powerupAmt + " power up!");
+        }
+    }
+}
