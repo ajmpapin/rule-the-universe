1b2c946d5771c28bda9465f597a93209094a6517
Ashley Papineau <design@ashleymuir.com>
2020-11-27 23:49:59 -0500

    Subtract stardust from player wallet

--- Assets/Scripts/LootStardust.cs
+++ Assets/Scripts/LootStardust.cs
@@ -3,22 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LootStardust : MonoBehaviour
-{
-
+public class LootStardust : MonoBehaviour {
     public Text stardustCount;
-
     int stardustAmt = 0;
 
     void OnTriggerEnter2D(Collider2D entity) {
-        Debug.Log ("Ka-ching!");
-
         if (entity.tag == "loot") {
+            Debug.Log ("Ka-ching!");
             Destroy(entity.gameObject);
-
             stardustAmt++;
             Debug.Log ("stardust = " + stardustAmt);
+            stardustCount.text = "Stardust Collected: " + stardustAmt;
+        }
+    }
 
+    public void BuyFriendship(int fee) {
+        if (stardustAmt <= 0) {
+            // don't take stardust, player is broke :(
+        } else {
+            Debug.Log ("stardust = " + stardustAmt);
+            stardustAmt = stardustAmt - fee;
             stardustCount.text = "Stardust Collected: " + stardustAmt;
         }
     }
